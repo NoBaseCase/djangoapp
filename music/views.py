8f219b0a940f7ee2fc152345ee8b332fa2decf40
nobasecase <rolandofuentes@Rolandos-MacBook-Pro.local>
2017-10-10 09:40:40 -0500

    added comments for clarification to views

--- music/views.py
+++ music/views.py
@@ -1,11 +1,20 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
 def index(request):
+	# grab all the albums from the database and assign them to an object
 	all_albums = Album.objects.all()
+	
+	# provide 'context' to the view that allows it to access to the object holding the data
 	context = {"all_albums" : all_albums,}	
+
+	# render the page, while passing it the data
 	return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-	return HttpResponse("<h2>details for album id: " + str(album_id) + "</h2>")
+	try:
+		album = Album.objects.get(pk=album_id)
+	except Album.DoesNotExist:
+		raise Http404(" Album does not exist")
+	return render(request, 'music/detail.html', {'album': album})
